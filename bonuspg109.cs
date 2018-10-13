5ea999b8a83fd83fe3fbc82a5a279ce715ecbd88
AEmenheiser <42221666+AEmenheiser@users.noreply.github.com>
2018-10-13 16:33:17 -0400

    Add files via upload

---
+++ bonuspg109.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+
+
+                Console.WriteLine("Please Enter a numeric grade from 0 to 100");
+                int numberGrade = int.Parse(Console.ReadLine());
+
+                if (numberGrade >= 98 && numberGrade <= 100)
+                {
+                    Console.WriteLine("A+");
+                }
+
+                else if (numberGrade >= 94 && numberGrade <= 97)
+                {
+                    Console.WriteLine("A");
+
+                }
+                else if (numberGrade >= 88 && numberGrade <= 93)
+                {
+                    Console.WriteLine("A-");
+
+                }
+                else if (numberGrade >= 86 && numberGrade <= 87)
+                {
+                    Console.WriteLine("b+");
+
+                }
+
+                else if (numberGrade >= 83 && numberGrade <= 86)
+                {
+                    Console.WriteLine("b");
+
+                }
+                else if (numberGrade >= 80 && numberGrade <= 82)
+                {
+                    Console.WriteLine("b-");
+
+                }
+
+                else if (numberGrade >= 77 && numberGrade <= 79)
+                {
+                    Console.WriteLine("c+");
+
+
+                }
+                else if (numberGrade >= 73 && numberGrade <= 76)
+                {
+                    Console.WriteLine("C");
+
+                }
+                else if (numberGrade >= 70 && numberGrade <= 72)
+                {
+                    Console.WriteLine("C-");
+
+                }
+                else if (numberGrade >= 68 && numberGrade <= 69)
+                {
+                    Console.WriteLine("D+");
+
+
+                }
+                else if (numberGrade >= 63 && numberGrade <= 67)
+                {
+                    Console.WriteLine("D");
+
+                }
+                else if (numberGrade >= 60 && numberGrade <= 62)
+                {
+                    Console.WriteLine("D-");
+
+                }
+                else if (numberGrade <= 59)
+                {
+                    Console.WriteLine("F");
+                }
+
+
+
+
+
+                Console.WriteLine("Would you like to continue? y/n");
+
+            } while (Console.ReadLine() == "y"); Console.WriteLine("have a good day");
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ real lab 2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            do
+            {
+
+                Console.WriteLine("Please enter the length of Room 1");
+                decimal room1length = decimal.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter the width of Room 1");
+                decimal room1width = decimal.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter Room Name");
+                string roomname = (Console.ReadLine());
+
+
+                decimal area1 = room1length * room1width;
+                decimal perimiter1 = room1length + room1width;
+
+                
+                Console.WriteLine("The area of " + roomname + " is " + area1);
+                Console.WriteLine("The perimiter of " + roomname + " is " + perimiter1);
+
+                
+
+
+
+               Console.ReadKey();
+
+                Console.WriteLine("Would you like to continue? y/n");
+               
+
+            } while (Console.ReadLine() == "y") ; Console.WriteLine("have a good day");
+            Console.ReadKey();
+
+        } 
+    }
+    }
+
+          
+
+
+      
+
+
+
+
+    
